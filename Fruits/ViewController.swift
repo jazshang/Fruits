348c0f57e6487fff2af8b3a8b5b7b2baa31c2380
jazshang <shang.sha@huksy.neu.edu>
2021-09-25 15:18:45 -0700

    Initial Commit

---
+++ Fruits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fruits
+//
+//  Created by Jasmine Shang on 9/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
